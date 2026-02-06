d0053ff5d2c977fbfaaabea7919a0fa4000bde8f
Josiah Sayles <Josiah.Sayles@gmail.com>
2026-02-06 13:05:19 -0700

    started building the architeture of the application as a monorepo including both server and client for simplicity, and time

---
+++ OntheRightTrack/Server/db/schema.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS users
+DROP TABLE IF EXISTS job_applications
+
+
+CREATE TABLE users (
+    id serial PRIMARY KEY, 
+    firstname text NOT NULL, 
+    lastname text NOT NULL, 
+    email text UNIQUE NOT NULL, 
+    password text NOT NULL
+);
+
+CREATE TABLE job_applications (
+    id serial PRIMARY KEY, 
+    companyname text NOT NULL, 
+    jobtitle text NOT NULL, 
+    location text NOT NULL, 
+    applicationdate date NOT NULL, 
+    status VARCHAR CHECK,
+    joblisting text, 
+    notest text, 
+    user_id interger NOT NULL REFERENCES users(id) ON DELETE CASCADE
+);
