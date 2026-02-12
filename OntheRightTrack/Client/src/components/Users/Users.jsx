a6e97e36f518dbe8b3e40a23da6e18fdeb089933
Josiah Sayles <Josiah.Sayles@gmail.com>
2026-02-12 16:22:27 -0700

    Starting building the Userpage adding react hooks needed to manage the different states of this page including user info, applications, adding, deleting, and updating applications and account information.

--- OntheRightTrack/Client/src/components/Users/Users.jsx
+++ OntheRightTrack/Client/src/components/Users/Users.jsx
@@ -1,5 +1,46 @@
-import React from "react";
+import { useState, useEffect, use } from "react";
+import { useNavigate } from "react-router";
+import { useAuth } from "../Auth/AuthContext";
 
 export default function Users() {
-  return <div>Users</div>;
+  const { token, logout } = useAuth();
+  const navigate = useNavigate();
+
+  const [user, setUser] = useState(null);
+  const [applications, setApplications] = useState([]);
+  const [editAccount, setEditAccount] = useState(false);
+  const [createApplication, setCreateApplication] = useState(false);
+  const [deleteApplication, setDeleteApplication] = useState(false);
+  const [editApplication, setEditApplication] = useState(false);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const applicationsAdded = applications.length;
+
+  useEffect(() => {
+    if (!token) {
+      navigate("/");
+      return;
+    }
+
+    const fetchData = async;
+  });
+  return (
+    <>
+      <section>
+        <div>
+          <h1>Welcome User</h1>
+        </div>
+      </section>
+      <section>
+        <div>
+          <h2>Applications</h2>
+        </div>
+      </section>
+      <section>
+        <div>
+          <h3>Manage your applications</h3>
+        </div>
+      </section>
+    </>
+  );
 }
