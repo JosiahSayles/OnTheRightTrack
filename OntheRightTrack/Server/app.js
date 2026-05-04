b178f7a167f86a6d05cfc08b41d104aef56e3d7d
Josiah Sayles <Josiah.Sayles@gmail.com>
2026-05-04 17:01:37 -0600

    updated  App.js on backend for CORS

--- OntheRightTrack/Server/app.js
+++ OntheRightTrack/Server/app.js
@@ -16,7 +16,7 @@ app.use(
     credentials: true,
   }),
 );
-app.options("*", cors());
+app.options("/", cors());
 app.use(express.json());
 app.use(getUserFromToken);
 app.use(express.urlencoded({ extended: true }));
