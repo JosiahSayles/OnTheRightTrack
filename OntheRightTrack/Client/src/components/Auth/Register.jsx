584554fff7973f2f85e8a1d735c02b4b982294bb
Josiah Sayles <Josiah.Sayles@gmail.com>
2026-05-04 16:03:45 -0600

    adjusting images on register page

--- OntheRightTrack/Client/src/components/Auth/Register.jsx
+++ OntheRightTrack/Client/src/components/Auth/Register.jsx
@@ -112,7 +112,7 @@ export default function Register() {
             </div>
           </div>
         </div>
-        <div className="flex justify-center items-center h-full ">
+        <div className="flex justify-center items-center h-1/2 ">
           <figure>
             <img
               src="/img/Interviewing.jpg"
