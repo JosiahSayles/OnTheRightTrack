92a48aa9aa57dedda6ddb509d5af5d990e16425b
Josiah Sayles <Josiah.Sayles@gmail.com>
2026-05-04 15:32:25 -0600

    adjusting images on register page

--- OntheRightTrack/Client/src/components/Auth/Register.jsx
+++ OntheRightTrack/Client/src/components/Auth/Register.jsx
@@ -112,17 +112,17 @@ export default function Register() {
             </div>
           </div>
         </div>
-        <div className="flex flex-col ">
+        <div className="flex flex-col w-1/4 ">
           <figure>
             <img
               src="/img/Interviewing.jpg"
               alt="hiring manager interviewing another man"
-              className="md:mx-1 mt-2 md:mt-10 md:w-1/4 shadow-md rounded-3xl "
+              className="md:mx-1 mt-2 md:mt-10  shadow-md rounded-3xl "
             />
             <img
               src="/img/Offer.jpg"
-              alt="hiring manager shaking hands with  potential female employee"
-              className="md:mx-1 mt-2  md:w-1/4 shadow-md rounded-3xl "
+              alt="hiring manager shaking hands with potential female employee"
+              className="md:mx-1 mt-2  shadow-md rounded-3xl "
             />
           </figure>
         </div>
